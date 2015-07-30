c33c87185883c280ce5276a523e4d02b63470541
Nelly Mincheva <nelly.m.mincheva@gmail.com>
2015-07-31 02:46:39 +0300

    Buggy xml loader...
    should be edited... a lot ._.

--- DatabaseApps-Team-Fluorescent-Pink/XmlLoader/XmlLoader.cs
+++ DatabaseApps-Team-Fluorescent-Pink/XmlLoader/XmlLoader.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Xml.XPath;
+using MsSql.Data;
+using MsSql.Models;
 
 namespace XmlLoader
 {
@@ -25,6 +27,29 @@ namespace XmlLoader
             }
 
             var nodes = xmlDocument.XPathSelectElements("//expenses-by-month/vendor");
+
+            foreach (var vendor in nodes)
+            {
+                string vendorName = vendor.Attribute("name").Value;
+                var expenses = vendor.Elements("expenses");
+
+                foreach (var expense in expenses)
+                {
+                    DateTime month = DateTime.Parse(expense.Attribute("month").Value);
+                    decimal amount = decimal.Parse(expense.Value);
+                    var context = new Supermarket(); // Nah... should be something else.. NOTE: Do NOT code at 2:40AM...
+                    var vendorId = context.Vendors
+                        .Where(v => v.VendorName.Trim() == vendorName)
+                        .Select(v => v.Id).First();
+
+                    Expense expenseEntity = new Expense();
+                    expenseEntity.VendorId = vendorId;
+                    expenseEntity.Date = expenseDate;
+                    expenseEntity.Amount = expenseAmount;
+                    context.Expenses.Add(expenseEntity);
+                    context.SaveChanges();
+                }
+            } 
 
             return true;
         }
